2786596116702d50f8ead18558a95e8dc3b49c43
Michael Wilson <michaelpwilson96@gmail.com>
2016-08-06 17:43:24 +0100

    Include only Angular2 related stuff and not every module in the project.

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -36,11 +36,17 @@ module.exports = function(grunt) {
 			files: [{
 				expand: true,
 				src: [
-					// './node_modules/es6-shim/es6-shim.min.js',
-					// './node_modules/systemjs/dist/system-polyfills.js',
-					// './node_modules/systemjs/dist/system.src.js',
-					// './node_modules/rxjs/bundles/Rx.js',
-					'./node_modules/**/*'
+				  './node_modules/es6-shim/es6-shim.min.js',
+				  './node_modules/systemjs/dist/system-polyfills.js',
+				  './node_modules/systemjs/dist/system.src.js',
+				  './node_modules/zone.js/dist/zone.js',
+				  './node_modules/reflect-metadata/Reflect.js',
+				  './node_modules/rxjs/**/*.js',
+				  './node_modules/@angular/core/bundles/core.umd.js',
+				  './node_modules/@angular/compiler/bundles/compiler.umd.js',
+				  './node_modules/@angular/common/bundles/common.umd.js',
+				  './node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+				  './node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
 				],
 				dest: '.tmp/public/js'
 			}]
