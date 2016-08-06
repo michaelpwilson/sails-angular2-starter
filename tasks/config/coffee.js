89fc3632fb72b48a050f7cb5a84dfeee967a4b27
Michael Wilson <michaelpwilson96@gmail.com>
2016-08-06 21:19:19 +0100

    Remove remaining merge comment

--- tasks/config/coffee.js
+++ tasks/config/coffee.js
@@ -33,39 +33,5 @@ module.exports = function(grunt) {
 			}]
 		}
 	});
-
 	grunt.loadNpmTasks('grunt-contrib-coffee');
-=======
- * `coffee`
- *
- * ---------------------------------------------------------------
- *
- * Compile CoffeeScript files located in `assets/js` into Javascript
- * and generate new `.js` files in `.tmp/public/js`.
- *
- * For usage docs see:
- *   https://github.com/gruntjs/grunt-contrib-coffee
- *
- */
-module.exports = function(grunt) {
-
-  grunt.config.set('coffee', {
-    dev: {
-      options: {
-        bare: true,
-        sourceMap: true,
-        sourceRoot: './'
-      },
-      files: [{
-        expand: true,
-        cwd: 'assets/js/',
-        src: ['**/*.coffee'],
-        dest: '.tmp/public/js/',
-        ext: '.js'
-      }]
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-coffee');
->>>>>>> 2ab7149ae458c2dffa2f4150ca1779133c1fa785
 };
